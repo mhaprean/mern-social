5c09502fceaed9c2d56a4d8dbd531c4f73bb0580
mhaprean <marianhaprean@gmail.com>
2023-04-05 16:56:47 +0300

    update comment design

--- client/src/components/comment/Comment.tsx
+++ client/src/components/comment/Comment.tsx
@@ -8,12 +8,18 @@ const Comment = ({ comment }: IPropsComment) => {
   return (
     <div className="flex space-x-2">
       <img src="https://img.freepik.com/free-icon/user_318-159711.jpg" alt="Profile" className="rounded-full w-10 h-10" />
-      <div>
-        <div className="flex space-x-1 flex-col">
-          <span className="font-bold">{comment.user.name}</span>
-          <span className="text-gray-400 font-bold text-xs">{comment.createdAt}</span>
+      <div className="flex flex-col">
+        <div className="rounded-2xl py-1 px-2 bg-slate-100 min-w-[120px]">
+          <div className="flex flex-col">
+            <span className="font-bold text-sm">{comment.user.name}</span>
+            {/* <span className="text-gray-400 font-bold text-xs">{comment.createdAt}</span> */}
+          </div>
+          <p className="text-gray-600 text-base">{comment.content}</p>
         </div>
-        <p className="text-gray-600">{comment.content}</p>
+        <div className="flex items-center gap-2 text-xs font-semibold">
+          <button className="text-gray-500 hover:text-gray-700 hover:underline">Like</button>
+          <button className="text-gray-500 hover:text-gray-700 hover:underline">Reply</button>
+        </div>
       </div>
     </div>
   );
